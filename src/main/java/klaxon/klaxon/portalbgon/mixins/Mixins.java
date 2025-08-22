e14eadaf8577ea99b9e14b05678a533abe2693ed
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-22 05:01:19 -0400

    port to new mixins system

--- src/main/java/klaxon/klaxon/portalbgon/mixins/Mixins.java
+++ src/main/java/klaxon/klaxon/portalbgon/mixins/Mixins.java
@@ -18,74 +18,25 @@
 
 package klaxon.klaxon.portalbgon.mixins;
 
-import static com.gtnewhorizon.gtnhlib.mixin.TargetedMod.VANILLA;
+import javax.annotation.Nonnull;
 
-import java.util.List;
-import java.util.function.Supplier;
-
-import com.gtnewhorizon.gtnhlib.mixin.IMixins;
-import com.gtnewhorizon.gtnhlib.mixin.ITargetedMod;
-import com.gtnewhorizon.gtnhlib.mixin.MixinBuilder;
-import com.gtnewhorizon.gtnhlib.mixin.Phase;
-import com.gtnewhorizon.gtnhlib.mixin.Side;
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
+import com.gtnewhorizon.gtnhmixins.builders.MixinBuilder;
 
 public enum Mixins implements IMixins {
 
-    NUKE_PORTAL_BLOCK(new MixinBuilder("Erase most of the methods inside BlockPortal").setApplyIf(() -> true)
-        .addMixinClasses("minecraft.MixinBlockPortal")
-        .addTargetedMod(VANILLA)
-        .setPhase(Phase.EARLY)
-        .setSide(Side.BOTH));
+    NUKE_PORTAL_BLOCK("Erase most of the methods inside BlockPortal", Side.COMMON, "minecraft.MixinBlockPortal");
 
-    private final List<String> mixinClasses;
-    private final Supplier<Boolean> applyIf;
-    private final Phase phase;
-    private final Side side;
-    private final List<ITargetedMod> targetedMods;
-    private final List<ITargetedMod> excludedMods;
+    private final MixinBuilder builder;
 
-    Mixins(MixinBuilder builder) {
-        mixinClasses = builder.mixinClasses;
-        applyIf = builder.applyIf;
-        side = builder.side;
-        targetedMods = builder.targetedMods;
-        excludedMods = builder.excludedMods;
-        phase = builder.phase;
-
-        if (mixinClasses.isEmpty()) throw new RuntimeException("No mixin class specified for Mixin : " + name());
-        if (targetedMods.isEmpty()) throw new RuntimeException("No targeted mods specified for Mixin : " + name());
-        if (applyIf == null) throw new RuntimeException("No ApplyIf function specified for Mixin : " + name());
-        if (phase == null) throw new RuntimeException("No Phase specified for Mixin : " + name());
-        if (side == null) throw new RuntimeException("No Side function specified for Mixin : " + name());
+    Mixins(String desc, Side side, String... classes) {
+        builder = new MixinBuilder(desc).addSidedMixins(side, classes)
+            .setPhase(Phase.EARLY);
     }
 
+    @Nonnull
     @Override
-    public List<String> getMixinClasses() {
-        return mixinClasses;
-    }
-
-    @Override
-    public Supplier<Boolean> getApplyIf() {
-        return applyIf;
-    }
-
-    @Override
-    public Phase getPhase() {
-        return phase;
-    }
-
-    @Override
-    public Side getSide() {
-        return side;
-    }
-
-    @Override
-    public List<ITargetedMod> getTargetedMods() {
-        return targetedMods;
-    }
-
-    @Override
-    public List<ITargetedMod> getExcludedMods() {
-        return excludedMods;
+    public MixinBuilder getBuilder() {
+        return builder;
     }
 }
